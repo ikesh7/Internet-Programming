f359ed477b6f56f2bbd05c7de04c1c273461c0c0
ikesh7 <ikesh28@gmail.com>
2024-05-15 23:19:28 +0100

    Update displayTeam.php
    Displaying data in descending order. Since added some extra column in the addTeam, displaying all of the extra column. Also added a button to display report

--- displayTeam.php
+++ displayTeam.php
@@ -5,25 +5,41 @@
 //connecting the database.
 require 'connect.php';
 //query to show all teams:
-	$stmt = "SELECT * FROM teams";
+	$stmt = "SELECT * FROM teams ORDER BY points DESC";
 	$result =$pdo->query($stmt);
 
         if ($result->rowCount() > 0) {
+            $cooks=1;
             echo "<h2>Teams Details</h2>";
+            echo "<form method='post' action='chartjs.php'>";
             echo "<table border='1'>";
-            echo "<tr><th>Team ID</th><th>Team Name</th><th>City</th><th>Manager</th><th>Total Players</th><th>Update</th><th>Delete</th></tr>";
+            echo "<tr><th>Select Teams</th><th>Team ID</th><th>Team Name</th><th>City</th><th>Manager</th><th>Games Played</th><th>Games won</th><th>Games drawn</th><th>Games lost</th><th>Games for</th><th>Games against</th><th>Goal difference</th><th>Points</th><th>Update</th><th>Delete</th></tr>";
             while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
                 echo "<tr>";
-                echo "<td>" . $row['teamId'] . "</td>";
+                echo "<td><input type='checkbox' name='team_id[]' value='" . $row['teamId'] . "'></td>";
+                echo "<td>" . $cooks . "</td>";
+                //echo "<td>" . $row['teamId'] . "</td>";
                 echo "<td>" . $row['name'] . "</td>";
                 echo "<td>" . $row['city'] . "</td>";
                 echo "<td>" . $row['manager'] . "</td>";
-                echo "<td>" . $row['totalPlayers'] . "</td>";
-                echo "<td>" . $row['totalPlayers'] . "</td>";
-               // echo "<td>" . "<li><a href=""updateTeam.php/$row['teamId']">Update</a></li>" . "</td>";
+                echo "<td>" . $row['played'] . "</td>";
+                echo "<td>" . $row['won'] . "</td>";
+                echo "<td>" . $row['drawn'] . "</td>";
+                echo "<td>" . $row['lost'] . "</td>";
+                echo "<td>" . $row['for'] . "</td>";
+                echo "<td>" . $row['against'] . "</td>";
+                echo "<td>" . $row['gd'] . "</td>";
+                echo "<td>" . $row['points'] . "</td>";
+                echo '<td><a href="updateTeam.php?editTeam_id='.$row['teamId'].'">Edit</a></td>';
+		echo '<td><a href="updateTeam.php?deleteTeam_id='.$row['teamId'].'">delete</a></td></tr>';
+                $cooks++;
+                
+               
                 echo "</tr>";
             }
             echo "</table>";
+            echo "<button type='submit'>Create Report</button>";
+    echo "</form>";
         } else {
             echo "No teams found.";
         }
