b3073bacf6f1ca1ff67d044705d21526ac7bc3c5
ikesh7 <ikesh28@gmail.com>
2024-05-19 18:33:12 +0100

    Update header.php
    updated the nav bar

--- header.php
+++ header.php
@@ -1,18 +1,23 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>New Football Team</title>
-        <link rel="stylesheet" href="layout.css">
-    </head>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Premier League Results</title>
+    <link rel="stylesheet" href="layout.css">
+</head>
+
+    <header>
+        <h3>CSYM019 - Premier League Results</h3>
+    </header>
+    <nav>
+        <ul>
+            <!--Nav bar to go to different pages -->
+            <li><a href="addTeam.php">Add New Football Team</a></li>
+            <li><a href="displayTeam.php">All Football Teams</a></li>
+            <li><a href="displayAdmin.php">All Registered users</a></li>
+            <li><a href="logout.php">Log out</a></li>
+        </ul>
+    </nav>
     <body>
-        <header>
-            <h3>CSYM019 - Premier League Results</h3>
-        </header>
-        <nav>
-            <ul>
-                <li><a href="./SelectionForm.html">Premier League Report</a></li>
-                <li><a href="./sampleEntryForm.html">Add New Football Team</a></li>
-                <li><a href="displayTeam.php">All Football Team</a></li>
-            </ul>
-        </nav>
-                
+        
