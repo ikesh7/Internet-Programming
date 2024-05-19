9f838a68bbbb934ec6272748928f4761e57a33c9
ikesh7 <ikesh28@gmail.com>
2024-05-19 18:28:35 +0100

    Update connect.php
    Some comments and updated the connect.php with try catch to handle better handle errors

--- connect.php
+++ connect.php
@@ -1,4 +1,9 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=assignment','root','');
-
+try {
+    // Create a new PDO instance to connect to the database where the host is the localhost and the database name from phpmyadmin is assignment
+    $pdo = new PDO('mysql:host=localhost;dbname=assignment', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Could not connect to the database: " . $e->getMessage());
+}
 ?>
